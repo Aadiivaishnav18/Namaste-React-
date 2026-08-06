261ae7170a92ac64e885a2f0b350d02435cfa952
Aditya Vaishnav <Aditya1807.arya@gmail.com>
2026-08-07 01:15:37 +0530

    Created the program to print hello Nameste from React and practice about to create nested elements

---
+++ Lecture_1/App.js
@@ -0,0 +1,27 @@
+// React program for the single element 
+
+//  const heading = React.createElement("h1", {id:"heading"}, "Hello World form the Namaste React !");
+
+// When the nested Element come like eg :-
+
+    // <div id="parent">
+    //     <div id="child">
+    //         <h1>
+                
+    //         </h1>
+    //     </div>
+    // </div>
+
+
+    const parent = React.createElement("div", {id:"parent"},
+        React.createElement("div", {id:"child"},
+            React.createElement("h1", {}, " I'am a h1 tag")
+        )
+    );
+
+
+
+ const root = ReactDOM.createRoot(document.getElementById("root"));
+
+ root.render(parent);
+
