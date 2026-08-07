cf639b3b4150e4e63bc7a5fca532c6f70d18fbd8
Aditya Vaishnav <Aditya1807.arya@gmail.com>
2026-08-07 23:59:13 +0530

    Complete the lecture 1st and learn about the write React code

--- Lecture_1/App.js
+++ Lecture_1/App.js
@@ -4,24 +4,86 @@
 
 // When the nested Element come like eg :-
 
-    // <div id="parent">
-    //     <div id="child">
-    //         <h1>
-                
-    //         </h1>
-    //     </div>
-    // </div>
+// <div id="parent">
+//     <div id="child">
+//         <h1>
+
+//         </h1>
+//     </div>
+// </div>
 
 
-    const parent = React.createElement("div", {id:"parent"},
-        React.createElement("div", {id:"child"},
-            React.createElement("h1", {}, " I'am a h1 tag")
-        )
-    );
+// const parent = React.createElement("div", { id: "parent" },
+//     React.createElement("div", { id: "child" },
+//         React.createElement("h1", {}, " I'am a h1 tag")
+//     )
+// );
+
+//-----------------Continue ------------------------------------//
+//    When the Sibling together Element come like eg :-
+
+// <div id="parent">
+//     <div id="child">
+//         <h1>
+//code 
+//         </h1>
+//   <h2>
+//    code
+//   </h2>
+//     </div>
+// </div>
+
+// made them into the array of the arrgument of child 
+
+
+// const parent = React.createElement("div", { id: "parent" },
+//     React.createElement("div", { id: "child" },
+//         [React.createElement("h1", {}, "Hello This is h1 tag"),
+//         React.createElement("h1", {}, "Hello This is h2 tag")]
+//     )
+// );
 
 
 
- const root = ReactDOM.createRoot(document.getElementById("root"));
+//-----------------Continue ------------------------------------//
+//    When the Sibling come together in parent  come like eg :-
 
- root.render(parent);
+// <div id="parent">
+//     <div id="child1">
+//         <h1>
+           //code 
+//         </h1>
+//   <h2>
+//    code
+//   </h2>
+//     </div>
+
+//     <div id="child2">
+//         <h1>
+           //code 
+//         </h1>
+//   <h2>
+//    code
+//   </h2>
+//     </div>
+// </div>
+
+// made them into the array of the arrgument of child 
+
+
+
+const parent = React.createElement("div", { id: "parent" },
+    [React.createElement("div", { id: "child1" },
+        [React.createElement("h1", {}, "Hello This is h1 tag"),
+        React.createElement("h2", {}, "Hello This is h2 tag")]
+    ),
+      React.createElement("div", { id: "child2" },
+        [React.createElement("h1", {}, "Hello This is h1 tag"),
+        React.createElement("h2", {}, "Hello This is h2 tag")]
+    )]
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
 
