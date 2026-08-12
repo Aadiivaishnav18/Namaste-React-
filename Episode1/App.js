5739606acd2db4d0e635c49e587f65ac4bd2a924
Aditya Vaishnav <Aditya1807.arya@gmail.com>
2026-08-12 15:28:48 +0530

    Completed the 2nd Episode (Ignition our App)

--- Episode1/App.js
+++ Episode1/App.js
@@ -1,89 +1,55 @@
-// React program for the single element 
 
-//  const heading = React.createElement("h1", {id:"heading"}, "Hello World form the Namaste React !");
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-// When the nested Element come like eg :-
+const parent = React.createElement(
+    "div",
+    { id: "parent" },
 
-// <div id="parent">
-//     <div id="child">
-//         <h1>
+    [
+        React.createElement(
+            "div",
+            { id: "child1", key: "child1" },
 
-//         </h1>
-//     </div>
-// </div>
+            [
+                React.createElement(
+                    "h1",
+                    { key: "h1-child1" },
+                    "Hello This is h1 tag"
+                ),
 
+                React.createElement(
+                    "h2",
+                    { key: "h2-child1" },
+                    "Hello This is h2 tag"
+                )
+            ]
+        ),
 
-// const parent = React.createElement("div", { id: "parent" },
-//     React.createElement("div", { id: "child" },
-//         React.createElement("h1", {}, " I'am a h1 tag")
-//     )
-// );
+        React.createElement(
+            "div",
+            { id: "child2", key: "child2" },
 
-//-----------------Continue ------------------------------------//
-//    When the Sibling together Element come like eg :-
+            [
+                React.createElement(
+                    "h1",
+                    { key: "h1-child2" },
+                    "Hello This is h1 tag"
+                ),
 
-// <div id="parent">
-//     <div id="child">
-//         <h1>
-//code 
-//         </h1>
-//   <h2>
-//    code
-//   </h2>
-//     </div>
-// </div>
-
-// made them into the array of the arrgument of child 
-
-
-// const parent = React.createElement("div", { id: "parent" },
-//     React.createElement("div", { id: "child" },
-//         [React.createElement("h1", {}, "Hello This is h1 tag"),
-//         React.createElement("h1", {}, "Hello This is h2 tag")]
-//     )
-// );
-
-
-
-//-----------------Continue ------------------------------------//
-//    When the Sibling come together in parent  come like eg :-
-
-// <div id="parent">
-//     <div id="child1">
-//         <h1>
-           //code 
-//         </h1>
-//   <h2>
-//    code
-//   </h2>
-//     </div>
-
-//     <div id="child2">
-//         <h1>
-           //code 
-//         </h1>
-//   <h2>
-//    code
-//   </h2>
-//     </div>
-// </div>
-
-// made them into the array of the arrgument of child 
-
-
-
-const parent = React.createElement("div", { id: "parent" },
-    [React.createElement("div", { id: "child1" },
-        [React.createElement("h1", {}, "Hello This is h1 tag"),
-        React.createElement("h2", {}, "Hello This is h2 tag")]
-    ),
-      React.createElement("div", { id: "child2" },
-        [React.createElement("h1", {}, "Hello This is h1 tag"),
-        React.createElement("h2", {}, "Hello This is h2 tag")]
-    )]
+                React.createElement(
+                    "h2",
+                    { key: "h2-child2" },
+                    "Hello This is h2 tag"
+                )
+            ]
+        )
+    ]
 );
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(
+    document.getElementById("root")
+);
 
 root.render(parent);
 
