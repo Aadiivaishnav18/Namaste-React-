e9e2c7800c6d69738eca77a6d172ef6ae96c331b
Aditya Vaishnav <Aditya1807.arya@gmail.com>
2026-09-04 16:42:38 +0530

    Fix shimmer loading and explain React state variables

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,22 +1,47 @@
 
 import RestrurantCard from "./RestaurantCards";
-import { use, useState } from "react";
+import { use, useEffect, useState } from "react";
 import resList from "../utils/mockData";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
 
-    const [listOfRestuarant, setListOfRestuarant] = useState(resList)
+    const [listOfRestuarant, setListOfRestuarant] = useState([])
 
-    return (
+    useEffect(() => {
+
+        const timer = setTimeout(() => {
+            setListOfRestuarant(resList);
+        }, 1000);
+
+        return () => clearTimeout(timer);
+
+    }, []);
+    // const fetchData = async () =>{
+    //     const data = await fetch(
+    //         "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.935192&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
+    //     );
+
+    //     const json = await data.json();
+    //     console.log(json);
+    //     setListOfRestuarant(json.data.cards[2].data.data.cards);
+    // }
+
+
+// By using Ternary operator :-
+
+    return listOfRestuarant.length === 0 ? (
+        < Shimmer />
+    ) : (
         <div id="body">
             <div className="filter">
                 <button className="filter-res"
-                onClick={()=>{
-   
-                   const filteredList = listOfRestuarant.filter((res)=> res.data.rating> 4.5);
-                
-                   setListOfRestuarant(filteredList)
-                }}
+                    onClick={() => {
+
+                        const filteredList = listOfRestuarant.filter((res) => res.data.rating > 4.5);
+
+                        setListOfRestuarant(filteredList)
+                    }}
                 > Top listed Restaurants </button>
             </div>
             <div className="res-container">
